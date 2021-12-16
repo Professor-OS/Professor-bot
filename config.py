98da41a9345b3efe4116a6c9a02867ac9a0c3cc2
Professor Agora <agoraprofessor@gmail.com>
2021-12-16 12:04:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = "" # 🌚 get it from @botfather (telegram) by sending /newbot command.
-    API_ID =  # 🌚 Same as APP_ID. Get it from my.telegram.org.
-    API_HASH = "" # 🌚 Get it from my.telegram.org.
+    BOT_TOKEN = "" 5017303003:AAHnGRi_79exb7AzsTVbUgQkXDXozJ3vEk0
+    API_ID =  "" 8117180
+    API_HASH = "" 34ce938c495ce1b2ae0e97fb237e8695
     
